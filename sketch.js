e37b4c78037c96b2ac2f474ba3f17b075f0599e5
Ashcc-python <54851478+Ashcc-python@users.noreply.github.com>
2021-10-25 17:09:16 +0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,30 @@
+var box;
+
+function setup() {
+  createCanvas(400,400);
+
+  box = createSprite(100,300,20,30) 
+  box.shapeColor = "blue"
+}
+
+function draw() 
+{
+  background(200,100,40);
+  if(keyIsDown(RIGHT_ARROW)){
+  box.x = box.x + 1
+  }
+  if(keyIsDown(LEFT_ARROW)){
+    box.x = box.x - 1
+  }
+  if(keyIsDown(UP_ARROW)){
+    box.y = box.y - 1
+  }
+  if(keyIsDown(DOWN_ARROW)){
+    box.y = box.y + 1
+  }
+  drawSprites();
+}
+
+
+
+
